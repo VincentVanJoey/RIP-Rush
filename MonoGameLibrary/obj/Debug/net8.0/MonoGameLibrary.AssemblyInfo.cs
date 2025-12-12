c2fc55a1c9536cf6ed949b655e20e53253d7df64
VincentVanJoey <107723566+VincentVanJoey@users.noreply.github.com>
2025-12-12 12:43:46 -0600

    deleting monogamelibrary that should have never been there

--- MonoGameLibrary/obj/Debug/net8.0/MonoGameLibrary.AssemblyInfo.cs
+++ MonoGameLibrary/obj/Debug/net8.0/MonoGameLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonoGameLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eece807eca11f1db190a2f125fa0956512d5dab2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+158cfed4905975cef96f8cc1bb0ca5a279209bfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonoGameLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonoGameLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
